130b89ffd5faf9c41adb64063f3c4f344a91d607
Rithik <Rithikvin.m@gmail.com>
2025-03-22 16:20:23 +0530

    forgot to add it to the stack

--- start.py
+++ start.py
@@ -1,2 +1,11 @@
-import numpy
+import openpyxl as xl
+wb = xl.load_workbook('transactions.xlsx')
+sheet = wb['Sheet1']
+lst = [1,2,3,4,5]
+
+for row in range(2,sheet.max_row+1):
+    cel = sheet.cell(row, 4)
+    cel.value = lst[row]
+
+wb.save("new.xlsx")
 
